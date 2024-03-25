dfde17baa7e7d8737163433274ae94cbcca4f6e9
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-03-25 21:09:24 +0000

    Deploying to gh-pages from @ Maiia11/goit-js-hw-09@f4d7dcd10d527277f59173a23a45c9189a8fd8c8 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";console.log("Form");
+import"./assets/modulepreload-polyfill-3cfb730f.js";const o=document.querySelector(".feedback-form");o.addEventListener("input",r);function r(t){const e=t.target.value.trim();localStorage.setItem(formEmail,e),localStorage.setItem(formMessage,e)}
 //# sourceMappingURL=commonHelpers2.js.map
