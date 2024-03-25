f4d7dcd10d527277f59173a23a45c9189a8fd8c8
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-03-25 23:09:06 +0200

    start task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,11 @@
-console.log("Form");
+const formKey = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+
+
+form.addEventListener("input", onLabelInput);
+
+function onLabelInput(event) {
+    const message = event.target.value.trim();
+    localStorage.setItem(formEmail, message);
+    localStorage.setItem(formMessage, message);
+}
