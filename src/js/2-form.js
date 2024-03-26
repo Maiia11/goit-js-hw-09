34afcf01f516b01356dcdbe7e9634816f9628dbf
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-03-26 10:20:16 +0200

    js-part task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,8 +4,34 @@ const form = document.querySelector(".feedback-form");
 
 form.addEventListener("input", onLabelInput);
 
-function onLabelInput(event) {
-    const message = event.target.value.trim();
-    localStorage.setItem(formEmail, message);
-    localStorage.setItem(formMessage, message);
+form.addEventListener("submit", handleSubmit);
+
+
+fillLabel();
+
+function handleSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(formKey);
+    
+}
+
+
+function onLabelInput() {
+    const saveInfo = {
+        email: form.email.value.trim(),
+        message: form.message.value.trim(),
+    };
+    localStorage.setItem(formKey, JSON.stringify(saveInfo));
+    
+}
+
+function fillLabel() {
+    const savedInform = JSON.parse(localStorage.getItem(formKey));
+
+    if (savedInform) {
+        const { email, message } = savedInform;
+        form.email.value = email;
+        form.message.value = message;
+    }
 }
